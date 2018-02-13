b424fe32cbb375f8dcd0e03095b210a01049de73
Rica Bruinsma <rbruinsma@madisoncollege.edu>
2018-02-13 10:56:02 -0600

    piecing together from week1hibernate revamp

---
+++ src/test/java/edu/matc/entity/UserTest.java
@@ -0,0 +1,29 @@
+package edu.matc.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserTest {
+/*
+    @Test
+    void getAge() {
+        // create the object that has the method I want to test
+        User user = new User();
+
+        // set birthdate for the user
+        LocalDate birthDate = LocalDate.parse("1968-01-01");
+        user.setUserDOB(birthDate);
+
+        // create a variable for the expected value
+        int expectedAge = 50;
+
+        // call the method, and get the actual value
+        int actualAge = user.getAge();
+
+        // compare the two, pass or fail
+        assertEquals(expectedAge, actualAge);
+    }  */
+}
---
+++ src/test/java/edu/matc/persistence/UserDaoTest.java
@@ -0,0 +1,119 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.User;
+import edu.matc.test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * The type User dao test.
+ */
+class UserDaoTest {
+
+
+    UserDao dao;
+
+    /**
+     * Creating the dao.
+     */
+    @BeforeEach
+    void setUp() {
+        dao = new UserDao();
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+    }
+
+    /**
+     * Verifies the gets all users successfuly.
+     */
+    @Test
+    void getAllUsersSuccess() {
+        List<User> users = dao.getAllUsers();
+        assertEquals(6, users.size());
+    }
+
+    /**
+     * Verifies get user by last name success.
+     */
+    @Test
+    void getUserByLastNameSuccess() {
+        List<User> users = dao.getUserByLastName("c");
+        assertEquals(3, users.size());
+    }
+
+    /**
+     * Verifies a user is returned correctly based on id search
+     */
+    @Test
+    void getByIdSuccess() {
+        User retrievedUser = dao.getById(3);
+        assertNotNull(retrievedUser);
+        assertEquals("Barney", retrievedUser.getFirstName());
+    }
+
+    /**
+     * Verify successful insert of a user
+     */
+    @Test
+    void insertSuccess() {
+
+        User newUser = new User("Fred", "Flintstone", "fflintstone", LocalDate.parse("1168-01-01"));
+        int id = dao.insert(newUser);
+        assertNotEquals(0,id);
+        User insertedUser = dao.getById(id);
+        assertEquals("Fred", insertedUser.getFirstName());
+        // Could continue comparing all values, but
+        // it may make sense to use .equals()
+        // TODO review .equals recommendations http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+    }
+
+    /**
+     * Verify successful delete of user
+     */
+    @Test
+    void deleteSuccess() {
+        dao.delete(dao.getById(3));
+        assertNull(dao.getById(3));
+    }
+
+    /**
+     * Verify successful update of user
+     */
+    @Test
+    void updateSuccess() {
+        String newLastName = "Davis";
+        User userToUpdate = dao.getById(3);
+        userToUpdate.setLastName(newLastName);
+        dao.saveOrUpdate(userToUpdate);
+        User retrievedUser = dao.getById(3);
+        assertEquals(newLastName, retrievedUser.getLastName());
+    }
+
+    /**
+     * Verify successful get by property (equal match)
+     */
+    @Test
+    void getByPropertyEqualSuccess() {
+        List<User> users = dao.getByPropertyLike("lastName", "Curry");
+        assertEquals(1, users.size());
+        assertEquals(3, users.get(0).getId());
+    }
+
+    /**
+     * Verify successful get by property (like match)
+     */
+    @Test
+    void getByPropertyLikeSuccess() {
+        List<User> users = dao.getByPropertyLike("lastName", "c");
+        assertEquals(3, users.size());
+    }
+}
