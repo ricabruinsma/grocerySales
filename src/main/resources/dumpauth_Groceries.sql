b7fd74db5c3323e14efd13698b9e072ab46bc33d
Rica Bruinsma <rbruinsma@madisoncollege.edu>
2018-03-20 14:17:04 -0500

    current database dump

---
+++ src/main/resources/dumpauth_Groceries.sql
@@ -0,0 +1,221 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: grocerysales
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` varchar(120) NOT NULL,
+  `category` varchar(120) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `size` varchar(120) DEFAULT NULL,
+  `price` decimal(4,2) NOT NULL,
+  `saleStart` date NOT NULL,
+  `saleEnd` date NOT NULL,
+  `updateDate` timestamp NOT NULL DEFAULT '2018-03-20 07:25:03',
+  `store_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Item_Store` (`store_id`),
+  CONSTRAINT `Item_Store` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `listline`
+--
+
+DROP TABLE IF EXISTS `listline`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `listline` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `quantity` int(3) NOT NULL,
+  `updateDate` timestamp NOT NULL DEFAULT '2018-03-20 07:25:03',
+  `shoppinglist_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ListLine_Item` (`item_id`),
+  KEY `ListLine_ShoppingList` (`shoppinglist_id`),
+  CONSTRAINT `ListLine_Item` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`),
+  CONSTRAINT `ListLine_ShoppingList` FOREIGN KEY (`shoppinglist_id`) REFERENCES `shoppinglist` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `listline`
+--
+
+LOCK TABLES `listline` WRITE;
+/*!40000 ALTER TABLE `listline` DISABLE KEYS */;
+/*!40000 ALTER TABLE `listline` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` varchar(120) NOT NULL,
+  `updateDate` timestamp NOT NULL DEFAULT '2018-03-20 07:25:03',
+  `username` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Role_User` (`username`),
+  KEY `role_user_id_fk` (`user_id`),
+  CONSTRAINT `role_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'2018-03-20 07:25:03','admin','2018-03-20 07:25:03','rbruinsma',1);
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shoppinglist`
+--
+
+DROP TABLE IF EXISTS `shoppinglist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shoppinglist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `totalAmount` decimal(4,2) NOT NULL,
+  `updateDate` timestamp NOT NULL DEFAULT '2018-03-20 07:25:03',
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ShoppingList_User` (`user_id`),
+  CONSTRAINT `ShoppingList_User` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shoppinglist`
+--
+
+LOCK TABLES `shoppinglist` WRITE;
+/*!40000 ALTER TABLE `shoppinglist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shoppinglist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` varchar(120) NOT NULL,
+  `address1` varchar(120) NOT NULL,
+  `address2` varchar(120) DEFAULT NULL,
+  `city` varchar(120) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `postalCode` varchar(16) NOT NULL,
+  `lat` decimal(9,6) NOT NULL,
+  `lon` decimal(9,6) NOT NULL,
+  `updateDate` timestamp NOT NULL DEFAULT '2018-03-20 07:25:03',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store`
+--
+
+LOCK TABLES `store` WRITE;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+INSERT INTO store VALUES (1, '2018-03-20 07:25:03', 'Hy-Vee', '3801 East Washington Avenue', NULL, 'Madison', 'WI', '53704', 43.117807, -89.317387, '2018-03-20 07:25:03');
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `address1` varchar(120) DEFAULT NULL,
+  `address2` varchar(120) DEFAULT NULL,
+  `city` varchar(120) DEFAULT NULL,
+  `state` varchar(2) DEFAULT NULL,
+  `postalCode` varchar(16) DEFAULT NULL,
+  `lat` decimal(9,6) DEFAULT NULL,
+  `lon` decimal(9,6) DEFAULT NULL,
+  `updateDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'2018-03-20 07:25:03','rbruinsma@madisoncollege.edu',NULL,'supersecret1','1701 Wright St.',NULL,'Madison','WI','53704',NULL,NULL,'2018-03-20 07:25:03','rbruinsma'),(2,'2018-03-20 07:25:03','testUser@madisoncollege.edu',NULL,'supersecret2','1701 Wright St.',NULL,'Madison','WI','53704',NULL,NULL,'2018-03-20 07:25:03','testUser'),(3,'2018-03-20 07:25:03','testUser2@madisoncollege.edu',NULL,'supersecret3','211 N. Carroll St.',NULL,'Madison','WI','53703',NULL,NULL,'2018-03-20 07:25:03','testUser2'),(30,NULL,NULL,NULL,'superSecret4',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2018-03-20 07:25:03','testUser4');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-20 13:43:55
